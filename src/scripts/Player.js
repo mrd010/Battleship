92131fa092e7ecfe498260ee2a22e0461aee5944
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-27 00:05:49 +0330

    Create Player modules

---
+++ src/scripts/Player.js
@@ -0,0 +1,21 @@
+import Gameboard from './Gameboard';
+
+class Player {
+  #gameboard;
+
+  constructor() {
+    this.#gameboard = new Gameboard();
+  }
+
+  getGameboard() {
+    return this.#gameboard;
+  }
+
+  //   attack(opponent, coordinates) {
+  //     if (!opponent.getGameboard().isValidShot(coordinates)) {
+  //       return { attacked: false };
+  //     }
+
+  //     return { attacked: true, shot: opponent.getGameboard().receiveAttack(coordinates) };
+  //   }
+}
