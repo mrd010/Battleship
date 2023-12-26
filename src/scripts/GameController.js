06bcb33279206e7acfced23bfacb73f8edea500b
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-27 00:54:58 +0330

    Create a module for control game flow

---
+++ src/scripts/GameController.js
@@ -0,0 +1,11 @@
+import Player from './Player';
+
+const startGame = function startGame() {
+  const player1 = new Player();
+  const player2 = new Player('ai');
+  let turn = player1;
+
+  while (true) {}
+};
+
+export default startGame;
