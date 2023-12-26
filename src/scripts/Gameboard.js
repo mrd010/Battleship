309623a49de09ac9d07320ed8206e47ac4eae055
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-26 20:15:28 +0330

    Create place ship method also now ships array. ships are created on placement time

--- src/scripts/Gameboard.js
+++ src/scripts/Gameboard.js
@@ -1,16 +1,34 @@
 import Ship from './Ship';
 
 class Gameboard {
-  // array of ship objects belong to this game board
-  #ships;
-
   //   2d array of board
   #board;
 
-  constructor(shipsSetup) {
-    this.#board = [[]];
-    // setup ships array
-    this.#ships = shipsSetup.map((shipType) => new Ship(shipType));
+  constructor() {
+    for (let x = 0; x < 10; x += 1) {
+      for (let y = 0; y < 10; y += 1) {
+        this.#board[x][y] = null;
+      }
+    }
+  }
+
+  #areEmpty(coordinatesArray) {
+    return coordinatesArray.every((coordinate) => this.#board[coordinate.x][coordinate.y] === null);
+  }
+
+  placeShip(shipLength, coordinatesArray) {
+    // if squares are occupied
+    if (!this.#areEmpty(coordinatesArray)) {
+      return false;
+    }
+
+    // if squares are empty
+    const ship = new Ship(shipLength);
+    coordinatesArray.forEach((coordinate) => {
+      // place ship in square
+      this.#board[coordinate.x][coordinate.y] = ship;
+    });
+    return true;
   }
 }
 
