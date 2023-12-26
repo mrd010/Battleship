6daddd88e392743bab1c40942f7ab4506aab2745
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-26 13:35:22 +0330

    Add comments

--- src/scripts/ShipSetup.js
+++ src/scripts/ShipSetup.js
@@ -2,6 +2,7 @@ import Ship from './Ship';
 
 // const shipsSetup = [2, 2, 2, 2, 3, 3, 3, 4, 4, 5];
 
+// create an array of ship object with given setup
 const createShips = function createShipsFromShipsSetup(shipsSetup) {
   return shipsSetup.map((shipType) => new Ship(shipType));
 };
