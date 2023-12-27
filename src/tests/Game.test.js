f5e4694445664a880d492fb766c29b4cd6de3082
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-27 20:36:25 +0330

    Test newly edited methods

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,2 +1,22 @@
 import Game from '../scripts/Game';
 import Player from '../scripts/Player';
+
+describe('Add player', () => {
+  const shipsSetup = [];
+  const battleshipsGame = new Game(shipsSetup);
+  test('Add first player', () => {
+    expect(battleshipsGame.addPlayer()).toBeInstanceOf(Player);
+  });
+  test('Add second player as ai', () => {
+    expect(battleshipsGame.addPlayer('ai')).toBeInstanceOf(Player);
+  });
+  test('max player is 2', () => {
+    expect(battleshipsGame.addPlayer()).toBeNull();
+  });
+  test('First Player is human', () => {
+    expect(battleshipsGame.player(1).isAI()).toBeFalsy();
+  });
+  test('SEcond Player is ai', () => {
+    expect(battleshipsGame.player(2).isAI()).toBeTruthy();
+  });
+});
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -11,3 +11,17 @@ describe('player creation', () => {
     expect(cpu.isAI()).toBeTruthy();
   });
 });
+
+describe('player got attacked', () => {
+  const player = new Player();
+
+  test('can attack at [5,6] first time', () => {
+    expect(player.receiveAttack([5, 6]).fired).toBeTruthy();
+  });
+  test('cant attack at [5,6] second time', () => {
+    expect(player.receiveAttack([5, 6]).fired).toBeFalsy();
+  });
+  test('cant attack at out of board second time', () => {
+    expect(player.receiveAttack([15, 6]).fired).toBeFalsy();
+  });
+});
