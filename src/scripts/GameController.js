5eb86c66f9eeafac13042affdc068c5d492de963
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-27 12:06:25 +0330

    Create place ship in game module gets player number and ship from input and can place it

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -16,6 +16,14 @@ class Game {
     this.#players.push(p);
     return p;
   }
+
+  placeShip(playerNumber, shipLength, coordinates) {
+    const pl = this.#players.find((player) => player.getNumber() === playerNumber);
+    if (pl === undefined) {
+      return false;
+    }
+    return pl.getGameboard().placeShip(shipLength, coordinates);
+  }
 }
 
 export default Game;
