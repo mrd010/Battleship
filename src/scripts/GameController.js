a1366e8763434db8eac55e880ff2de0fea2f7999
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-27 11:51:33 +0330

    Create an add player to game

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -1,1 +1,21 @@
 import Player from './Player';
+
+class Game {
+  #players;
+
+  #turn;
+
+  constructor() {
+    this.#players = [];
+    this.#turn = 0;
+  }
+
+  // create and assign a new player to game
+  newPlayer(playerType) {
+    const p = new Player(playerType);
+    this.#players.push(p);
+    return p;
+  }
+}
+
+export default Game;
