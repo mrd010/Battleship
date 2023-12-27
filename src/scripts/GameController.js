e31ce30d2db49d3f99d961d850a4dbebd9958247
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-27 11:40:39 +0330

    Reset module for DOM interaction

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -1,11 +1,1 @@
 import Player from './Player';
-
-const startGame = function startGame() {
-  const player1 = new Player();
-  const player2 = new Player('ai');
-  let turn = player1;
-
-  while (true) {}
-};
-
-export default startGame;
