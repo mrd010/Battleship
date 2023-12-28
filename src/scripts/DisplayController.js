6cb6c439c4cbdc0e1d1119dd13eb4745050f444e
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-28 13:07:24 +0330

    Add play screen page and load

--- src/scripts/DisplayController.js
+++ src/scripts/DisplayController.js
@@ -1,1 +1,29 @@
+import { getGameDefaults } from './GameController';
+import { createPlayScreen, createStartScreen } from './TemplateCreator';
 
+const loadPlayScreen = function loadPlayScreen() {
+  // create play screen with default settings
+  const gameDefaults = getGameDefaults();
+  const playScreen = createPlayScreen(gameDefaults.shipsSetup, gameDefaults.boardWidth);
+
+  // hide starting screen page
+  document.getElementById('start-screen').remove();
+  document.body.appendChild(playScreen);
+  document.querySelectorAll('#player-board .game-board button').forEach((button) => {
+    button.addEventListener('click', () => {
+      console.log(`[${button.getAttribute('data-x')},${button.getAttribute('data-y')}]`);
+    });
+  });
+};
+
+const initDisplay = function initDisplay() {
+  // create start screen
+  document.body.setAttribute('class', 'bg-gradient-to-b h-screen from-slate-950 to-slate-900');
+  const startScreen = createStartScreen();
+  document.body.appendChild(startScreen);
+
+  // start button loads game
+  startScreen.querySelector('#start-button').addEventListener('click', loadPlayScreen);
+};
+
+export default initDisplay;
